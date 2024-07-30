7e17817a877fbb77257a2e9856b841944150c5be
Joseph Kariuki <joehene@gmail.com>
2024-07-30 10:08:14 +0300

    Registered user model to admin

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import CustomUser
 
-# Register your models here.
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email')
+    list_display_links = ('username', 'email')
+    prepopulated_fields = {'username': ('username',)}

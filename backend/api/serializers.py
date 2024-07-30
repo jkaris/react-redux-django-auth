026761be4aba88d8b8d6a3adf5bb65093e557d78
Joseph Kariuki <joehene@gmail.com>
2024-07-30 10:07:29 +0300

    Added serializers

---
+++ backend/api/serializers.py
@@ -0,0 +1,34 @@
+from django.conf import settings
+from rest_framework import serializers
+from .models import CustomUser
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """
+    Serializer for User model
+    """
+    class Meta:
+        model = CustomUser
+        fields = ('id', 'username', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        """
+        Create new user with encrypted password and return it
+        :param validated_data:
+        :return:
+        """
+        user = CustomUser(
+            email=validated_data['email'],
+            username=validated_data['username']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+    class Meta:
+        """
+        Serializer for User model
+        """
+        model = CustomUser
+        fields = ('id', 'username', 'email')
